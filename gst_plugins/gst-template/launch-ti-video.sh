0e0ea39e4497bc9c2e115bc7bdd6b596e01a2149
Kamil Piekutowski <piekutowskik@cumminsallison.com>
2020-01-28 17:00:52 +0000

    added cavideoconverter

--- gst_plugins/gst-template/launch-ti-video.sh
+++ gst_plugins/gst-template/launch-ti-video.sh
@@ -1,5 +1,5 @@
 if [ "$1" == "DEBUG" ]; then
-	GST_DEBUG=5 gst-launch-1.0 filesrc location=TearOfSteel-Short-1920x800.mov ! qtdemux name=demux demux.video_0 ! queue ! h264parse ! ducatih264dec ! myfilter ! waylandsink
+	GST_DEBUG=5 gst-launch-1.0 filesrc location=TearOfSteel-Short-1920x800.mov ! qtdemux name=demux demux.video_0 ! queue ! h264parse ! ducatih264dec ! cavideoconverter ! waylandsink
 else
-	gst-launch-1.0 filesrc location=TearOfSteel-Short-1920x800.mov ! qtdemux name=demux demux.video_0 ! queue ! h264parse ! ducatih264dec ! myfilter ! waylandsink 
+	gst-launch-1.0 filesrc location=TearOfSteel-Short-1920x800.mov ! qtdemux name=demux demux.video_0 ! queue ! h264parse ! ducatih264dec ! cavideoconverter ! waylandsink 
 fi
